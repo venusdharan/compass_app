e0e89334a0616d7ef2d10aad75a053372fb90ecd
venusdharan <venusdharan@gmail.com>
2021-09-23 11:47:29 +0530

    init

---
+++ src/main/java/com/triophore/compass/app/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.triophore.compass.app.screen.user;
+
+import com.triophore.compass.app.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/com/triophore/compass/app/CompassappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triophore.compass.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
